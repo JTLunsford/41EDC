e4972342b59909e74eb3d8f0519e3f66305ad6d5
Joshua Lunsford <josh@decodingideas.com>
2017-01-15 03:25:55 +0000

    Lots of updates

--- client/dungeon.js
+++ client/dungeon.js
@@ -2,17 +2,22 @@ module.exports = (game)=>{
     let tile = game.assets['s'].textures['HalfBrick0'];
     tile.baseTexture.scaleMode = game.pixi.SCALE_MODES.NEAREST;
     
-    game.halfbricks = new game.pixi.particles.ParticleContainer();
-    // game.halfbricks.position.x = 0;
-    // game.halfbricks.position.y = 0;
+    game.halfbricks = new game.pixi.particles.ParticleContainer((game.config.world.width/8)*(game.config.world.width/8), {
+        scale: true,
+        position: true,
+        rotation: true,
+        uvs: true,
+        alpha: true
+    });
+    game.halfbricks.position.x = 0;
+    game.halfbricks.position.y = 0;
     game.halfbricks.width = game.config.world.width;
     game.halfbricks.height = game.config.world.height;
     
-    for (let x = 0; x < 8 * 8 * 8 * 8; x=x+8)
+    for (let x = 0; x < game.config.world.width; x=x+8)
     {
-        for (let y = 0; y < 8 * 8 * 8 * 8; y=y+8)
+        for (let y = 0; y < game.config.world.height; y=y+8)
         {
-            //console.log(x,y);
             let brick = new game.pixi.Sprite(tile);
             brick.position.x = x;
             brick.position.y = y;
